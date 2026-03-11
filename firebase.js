64f62d00dad3bd2ca42bd7e45426364ba72123f3
ATE837 <yawruda@gmail.com>
2026-03-11 22:12:19 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,13 +1,19 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
 const firebaseConfig = {
-
-apiKey: "BURAYA",
-authDomain: "BURAYA",
-databaseURL: "BURAYA",
-projectId: "BURAYA",
-storageBucket: "BURAYA",
-messagingSenderId: "BURAYA",
-appId: "BURAYA"
-
+  apiKey: "AIzaSyCwwqd4FfhvLRQu8DUUfbdorIu3iJpkHMM",
+  authDomain: "ateschat-cd9f4.firebaseapp.com",
+  databaseURL: "https://ateschat-cd9f4-default-rtdb.firebaseio.com",
+  projectId: "ateschat-cd9f4",
+  storageBucket: "ateschat-cd9f4.firebasestorage.app",
+  messagingSenderId: "174732212740",
+  appId: "1:174732212740:web:dcd4b60ed7cc380ca95351",
+  measurementId: "G-1CBZNR0W3E"
 };
 
-firebase.initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const provider = new GoogleAuthProvider();
