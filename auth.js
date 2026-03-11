3097a107d8b1d05140b0e46fa42da5f8c352299c
ATE837 <yawruda@gmail.com>
2026-03-11 21:30:37 +0300

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,53 +1,23 @@
 function register(){
 
-let email = document.getElementById("email").value
-let password = document.getElementById("password").value
+let email=document.getElementById("email").value
+let password=document.getElementById("password").value
 
-auth.createUserWithEmailAndPassword(email,password)
-
-.then(()=>{
-
-alert("Kayıt başarılı")
-
-})
-
-.catch((error)=>{
-
-alert(error.message)
-
-})
+firebase.auth().createUserWithEmailAndPassword(email,password)
 
 }
-
-
 
 function login(){
 
-let email = document.getElementById("email").value
-let password = document.getElementById("password").value
+let email=document.getElementById("email").value
+let password=document.getElementById("password").value
 
-auth.signInWithEmailAndPassword(email,password)
-
-.then(()=>{
-
-document.getElementById("login").style.display="none"
-document.getElementById("app").style.display="block"
-
-})
-
-.catch((error)=>{
-
-alert(error.message)
-
-})
+firebase.auth().signInWithEmailAndPassword(email,password)
 
 }
 
-
-
 function logout(){
 
-auth.signOut()
-location.reload()
+firebase.auth().signOut()
 
 }
