361b389423dfe27843a4968b16189039c7f4d79e
ATE837 <yawruda@gmail.com>
2026-03-11 21:57:59 +0300

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,23 +1,47 @@
 function register(){
 
 let email=document.getElementById("email").value
-let password=document.getElementById("password").value
+let pass=document.getElementById("password").value
+let username=document.getElementById("username").value
 
-firebase.auth().createUserWithEmailAndPassword(email,password)
+firebase.auth().createUserWithEmailAndPassword(email,pass)
+.then(user=>{
+
+firebase.database().ref("users/"+user.user.uid).set({
+username:username
+})
+
+})
 
 }
 
 function login(){
 
 let email=document.getElementById("email").value
-let password=document.getElementById("password").value
+let pass=document.getElementById("password").value
 
-firebase.auth().signInWithEmailAndPassword(email,password)
+firebase.auth().signInWithEmailAndPassword(email,pass)
 
 }
 
 function logout(){
-
 firebase.auth().signOut()
+}
+
+firebase.auth().onAuthStateChanged(user=>{
+
+if(user){
+
+document.getElementById("loginPage").style.display="none"
+document.getElementById("chatPage").style.display="block"
+
+loadMessages()
+
+}else{
+
+document.getElementById("loginPage").style.display="block"
+document.getElementById("chatPage").style.display="none"
 
 }
+
+})
