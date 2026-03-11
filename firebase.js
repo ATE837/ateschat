1f9fae3f3a7b515b320b31a2ca724656d6b316cd
ATE837 <yawruda@gmail.com>
2026-03-11 18:16:03 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,15 @@
+const firebaseConfig = {
 
+apiKey: "AIzaSyCwwqd4FfhvLRQu8DUUfbdorIu3iJpkHMM",
+authDomain: "ateschat-cd9f4.firebaseapp.com",
+databaseURL: "https://ateschat-cd9f4-default-rtdb.firebaseio.com",
+projectId: "ateschat-cd9f4",
+storageBucket: "ateschat-cd9f4.firebasestorage.app",
+messagingSenderId: "174732212740",
+appId: "1:174732212740:web:dcd4b60ed7cc380ca95351"
+
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.database();
