4bd619032904c84ac076a2330784cf4927de524e
ATE837 <yawruda@gmail.com>
2026-03-11 19:15:49 +0300

    Update ui.js

--- ui.js
+++ ui.js
@@ -1,1 +1,19 @@
+// SAYFA AÇILINCA
 
+window.onload = function(){
+
+let user = localStorage.getItem("user")
+
+if(user){
+
+document.getElementById("login").style.display = "none"
+document.getElementById("app").style.display = "block"
+
+}else{
+
+document.getElementById("login").style.display = "flex"
+document.getElementById("app").style.display = "none"
+
+}
+
+}
