e3d9149ddcfe3a5fca2473d1b9df4f0a44195d5b
ATE837 <yawruda@gmail.com>
2026-03-11 20:52:03 +0300

    Update chat.js

--- chat.js
+++ chat.js
@@ -1,1 +1,34 @@
+let room = "global"
 
+function send(){
+
+let user = auth.currentUser.email
+let msg = document.getElementById("messageInput").value
+
+if(msg === "") return
+
+db.ref("messages/"+room).push({
+
+user:user,
+text:msg,
+time:Date.now()
+
+})
+
+document.getElementById("messageInput").value=""
+
+}
+
+
+
+db.ref("messages/global").on("child_added",function(snapshot){
+
+let data = snapshot.val()
+
+let box = document.createElement("div")
+
+box.innerText = data.user + " : " + data.text
+
+document.getElementById("messages").appendChild(box)
+
+})
