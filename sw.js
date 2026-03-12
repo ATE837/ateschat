05f29114e63b69943cf3f11e7a2b48e431c5c4e1
ATE837 <yawruda@gmail.com>
2026-03-12 05:48:35 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE = 'ateschat-v1';
+const FILES = [
+  '/ateschat/',
+  '/ateschat/index.html',
+  '/ateschat/style.css',
+  '/ateschat/app.js',
+  '/ateschat/manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
