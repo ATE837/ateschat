91d6ceee8fbed6f67b59805d382f207f409863e4
ATE837 <yawruda@gmail.com>
2026-03-14 00:53:07 +0300

    firebase.js

--- firebase.js
+++ firebase.js
@@ -1,22 +1,22 @@
-// Firebase yapılandırması - SENİN ANAHTARLARIN
+// Firebase yapılandırması
 const firebaseConfig = {
-  apiKey: "AIzaSyCwwqd4FfhvLRQu8DUUfbdorIu3iJpkHMM",
-  authDomain: "ateschat-cd9f4.firebaseapp.com",
-  databaseURL: "https://ateschat-cd9f4-default-rtdb.firebaseio.com",
-  projectId: "ateschat-cd9f4",
-  storageBucket: "ateschat-cd9f4.firebasestorage.app",
-  messagingSenderId: "174732212740",
-  appId: "1:174732212740:web:dcd4b60ed7cc380ca95351",
-  measurementId: "G-1CBZNR0W3E"
+    apiKey: "AIzaSyCwwqd4FfhvLRQu8DUUfbdorIu3iJpkHMM",
+    authDomain: "ateschat-cd9f4.firebaseapp.com",
+    databaseURL: "https://ateschat-cd9f4-default-rtdb.firebaseio.com",
+    projectId: "ateschat-cd9f4",
+    storageBucket: "ateschat-cd9f4.firebasestorage.app",
+    messagingSenderId: "174732212740",
+    appId: "1:174732212740:web:dcd4b60ed7cc380ca95351",
+    measurementId: "G-1CBZNR0W3E"
 };
 
 // Firebase'i başlat
 firebase.initializeApp(firebaseConfig);
 
-// Servisleri global değişkenlere ata
+// Servisler
 const auth = firebase.auth();
 const db = firebase.firestore();
 const storage = firebase.storage();
 const timestamp = firebase.firestore.FieldValue.serverTimestamp;
 
-console.log("✅ Firebase başarıyla başlatıldı!");
+console.log("✅ Firebase başlatıldı");
